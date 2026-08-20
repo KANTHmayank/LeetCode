803aa8bff06d3017364dc84e09b97ad325e334a1
Mayank Kanth <143795141+KANTHmayank@users.noreply.github.com>
2026-08-20 17:01:48 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (92.92%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -2,37 +2,39 @@ class Solution {
     List<String> result = new ArrayList<>();
     public List<String> generateParenthesis(int n) {
         StringBuilder curr = new StringBuilder();
-
-        solve(curr, n);
+        int open = 0;
+        int close = 0;
+        solve(curr, n, open, close);
         return result;
     }
 
-    private void solve(StringBuilder curr, int n){
+    private void solve(StringBuilder curr, int n, int open, int close){
         if(curr.length() == 2*n){
-            if(isValid(curr)){
-                result.add(curr.toString());
-
-            }
+            result.add(curr.toString());
             return;
         }
 
+        if(open < n){
         curr.append('(');
-        solve(curr, n);
+        solve(curr, n, open + 1, close);
         curr.deleteCharAt(curr.length() - 1);
-
-        curr.append(')');
-        solve(curr, n);
-        curr.deleteCharAt(curr.length() - 1);
-    }
-
-    private boolean isValid(StringBuilder str){
-        int count = 0;
-        for(char ch : str.toString().toCharArray()){
-            if(ch == '(') count++;
-            else count--;
-            if(count < 0) return false; 
         }
 
-        return count == 0;
+        if(close < open){
+        curr.append(')');
+        solve(curr, n, open, close + 1);
+        curr.deleteCharAt(curr.length() - 1);
+        }
     }
+
+    // private boolean isValid(StringBuilder str){
+    //     int count = 0;
+    //     for(char ch : str.toString().toCharArray()){
+    //         if(ch == '(') count++;
+    //         else count--;
+    //         if(count < 0) return false; 
+    //     }
+
+    //     return count == 0;
+    // }
 }
