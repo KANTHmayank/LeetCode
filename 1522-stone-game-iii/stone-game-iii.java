c8beb9b81e3cbdedffb07d25105b3b29ab960910
Mayank Kanth <143795141+KANTHmayank@users.noreply.github.com>
2026-08-21 09:50:16 +0530

    Time: 6 ms (92.37%) | Memory: 86.8 MB (26.78%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -1,37 +1,50 @@
 class Solution {
     public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] t = new int[n+1];
+        // Arrays.fill(t, -1);
 
-        int[] dp = new int[stoneValue.length];
-        Arrays.fill(dp, -1);
+        for(int i = n-1; i >= 0; i--){
+            
+            t[i] = stoneValue[i] - t[i+1];
+            
 
+            if(i + 2 <= n){
+                t[i] = Math.max(t[i], stoneValue[i] + stoneValue[i+1] - t[i+2]);
+            }
+            if(i + 3 <= n){
+                t[i] = Math.max(t[i], stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - t[i+3]);
+            }
+        }
+        int diff = t[0];
         
-        if(solve(stoneValue, 0, dp) > 0){
+        if(diff > 0){
             return "Alice";
-        }else if(solve(stoneValue, 0, dp) < 0){
+        }else if(diff< 0){
             return "Bob";
         }else{
             return "Tie";
         }
     }
 
-    private int solve(int[] stoneValue, int i, int[] dp){
-        int n = stoneValue.length;
-        if(i >= n) return 0;
+    // private int solve(int[] stoneValue, int i, int[] dp){
+    //     int n = stoneValue.length;
+    //     if(i >= n) return 0;
 
-        if(dp[i] != -1) return dp[i];
+    //     if(dp[i] != -1) return dp[i];
 
-        int result = Integer.MIN_VALUE;
+    //     int result = Integer.MIN_VALUE;
 
-        result = Math.max(result, stoneValue[i] - solve(stoneValue, i + 1, dp));
+    //     result = Math.max(result, stoneValue[i] - solve(stoneValue, i + 1, dp));
 
-        if(i + 1 < n){
-            result = Math.max(result, stoneValue[i] + stoneValue[i + 1] - solve(stoneValue, i + 2, dp));
-        }
+    //     if(i + 1 < n){
+    //         result = Math.max(result, stoneValue[i] + stoneValue[i + 1] - solve(stoneValue, i + 2, dp));
+    //     }
 
-        if(i + 2 < n){
-            result = Math.max(result, stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - solve(stoneValue, i + 3, dp));
-        }
+    //     if(i + 2 < n){
+    //         result = Math.max(result, stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - solve(stoneValue, i + 3, dp));
+    //     }
 
-        return dp[i] = result;
-    }
+    //     return dp[i] = result;
+    // }
 }
