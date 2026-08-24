ee270a225a684393df481cb78279f1ad6d930c88
Mayank Kanth <143795141+KANTHmayank@users.noreply.github.com>
2026-08-24 14:43:38 +0530

    Time: 7 ms (26.38%) | Memory: 86.6 MB (72.34%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.java
+++ 2002-stone-game-viii/stone-game-viii.java
@@ -11,19 +11,29 @@ class Solution {
             prefixSum[i] = prefixSum[i - 1] + stones[i];
         }
 
-        int[] dp = new int[n];
-        Arrays.fill(dp, Integer.MIN_VALUE);
-        return solve(1, dp, prefixSum);
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, 0);
+        // return solve(1, dp, prefixSum);
+
+        dp[n - 1] = prefixSum[n - 1];
+        for(int i = n - 2; i >= 1; i--){
+            int take = prefixSum[i] - dp[i + 1];
+            int skip = dp[i + 1];
+
+            dp[i] = Math.max(take, skip);
+        }
+
+        return dp[1];
     }
 
-    private int solve(int i, int[] dp, int[] prefixSum){
-        if(i == n-1) return dp[i] = prefixSum[n - 1];
+    // private int solve(int i, int[] dp, int[] prefixSum){
+    //     if(i == n-1) return dp[i] = prefixSum[n - 1];
 
-        if(dp[i] != Integer.MIN_VALUE) return dp[i];
+    //     if(dp[i] != Integer.MIN_VALUE) return dp[i];
 
-        int take = prefixSum[i] - solve(i + 1, dp, prefixSum);
-        int skip = solve(i + 1, dp, prefixSum);
+    //     int take = prefixSum[i] - solve(i + 1, dp, prefixSum);
+    //     int skip = solve(i + 1, dp, prefixSum);
 
-        return dp[i] = Math.max(take, skip);
-    }
+    //     return dp[i] = Math.max(take, skip);
+    // }
 }
